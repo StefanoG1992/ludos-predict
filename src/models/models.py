19e912b7a7fb28e5825c0198666903a440dbc30c
c332192 <stefano.gioffre2@unicredit.eu>
2022-11-16 08:21:19 +0100

    Created encoding plot

--- src/models/models.py
+++ src/models/models.py
@@ -1,5 +1,84 @@
 """Models module
 
-This module contains all self-made ML models for data analysis purposes
+This module contains all self-made ML models for data science purposes
 """
+
+import logging
+import shap
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.preprocessing import StandardScaler
+from catboost import CatBoostClassifier
+
+from typing import List
+
+
+# initialize logger
+logger = logging.getLogger(__name__)
+
+
+# TODO Boilerplate code with plot.py, fix it
+def get_shap_top_features(
+        X: pd.DataFrame,
+        y: pd.Series,
+        n_top: int,
+) -> List[str]:
+    logger.info(f"Computing {n_top} best features")
+
+    # split values
+    X_train, X_val, y_train, y_val = train_test_split(
+        X, y, train_size=0.7, shuffle=True, stratify=y
+    )
+
+    # rescale values as CatBoost is scaling-sensitive
+    scl = StandardScaler()
+    X_train_scl = scl.fit_transform(X_train)
+    X_val_scl = scl.transform(X_val)
+
+    logger.info("Training model")
+    # simple model
+    mdl = CatBoostClassifier(allow_writing_files=False)
+    mdl.fit(
+        X=X_train_scl,
+        y=y_train,
+        verbose=0,
+        eval_set=(X_val_scl, y_val),
+        use_best_model=True
+    )
+
+    logger.info("Explaining data")
+
+    # shap explainer
+    explainer = shap.TreeExplainer(mdl)
+    # shap values are a list
+    shap_values: List = explainer.shap_values(X_train_scl)
+
+    # generate feature importances df
+    shap_array = np.array(shap_values)  # shape = (# classes, # data, # features)
+    logger.debug(f"Shapley coefficients array has shape: {shap_array.shape}")
+
+    # retrieve absolute importance
+    shap_array = np.abs(shap_array)  # get absolute importance
+    shap_array = shap_array.mean(1)  # shape (# classes, # features)
+
+    # get it into a df
+    features_df = pd.DataFrame(shap_array)
+    features_df.columns = X.columns
+
+    # find best features - summing on all classes and sorting
+    best_features: pd.Series = features_df.sum().sort_values(ascending=False)
+
+    # taking first n values
+    best_n_features = best_features.loc[:n_top]
+
+    return best_n_features
+
+
+
+
+
 
--- src/models/plot.py
+++ src/models/plot.py
@@ -9,12 +9,15 @@ import logging
 import shap
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import StandardScaler
 from catboost import CatBoostClassifier
+from sklearn.neural_network import MLPRegressor
 
 
 # initialize logger
@@ -75,27 +78,31 @@ def class_frequency(
 def print_shap_values(
         X: pd.DataFrame,
         y: pd.Series,
-        save_dir: Path
+        save_dir: Path,
 ) -> None:
     """
     Plot Shapley feature importance using tree model.
+
+    :param X: Explanatory data as df
+    :param y: Classes as series
+    :param save_dir: directory where to save results
     """
     # start
     logger.info("Computing Shapley values.")
-    title = "Shapley Values"
+    title = "shapley_values"
 
     # split values
     X_train, X_val, y_train, y_val = train_test_split(
         X, y, train_size=0.7, shuffle=True, stratify=y
     )
 
-    # rescale values
+    # rescale values as CatBoost is sensitive
     scl = StandardScaler()
     X_train_scl = scl.fit_transform(X_train)
     X_val_scl = scl.transform(X_val)
 
     # simple model
-    mdl = CatBoostClassifier()
+    mdl = CatBoostClassifier(allow_writing_files=False)
     mdl.fit(
         X=X_train_scl,
         y=y_train,
@@ -115,14 +122,106 @@ def print_shap_values(
         class_names=mdl.classes_,
         plot_type="bar",
         show=False,
-        plot_size=(7, 8),
+        plot_size=(10, 20),
     )
     plt.grid(linestyle=":")
 
-    # save
-    save_path = save_dir / f"{title}.png"
-    plt.savefig(save_path)
+    # save image
+    image_path = save_dir / f"{title}.png"
+    plt.savefig(image_path)
     plt.close()
 
     # end
-    logger.info(f"Shapley summary plot saved in {save_path}")
+    logger.info(f"Shapley summary info saved in {save_dir}")
+
+
+def encoder(
+        X: pd.DataFrame | np.ndarray | list,
+        reg: MLPRegressor
+) -> np.ndarray:
+    """
+    Encode data in 2D.
+
+    :param X: explanatory data
+    :param reg: trained model
+    :return:
+    """
+    X = np.asmatrix(X)
+
+    encoder1 = X * reg.coefs_[0] + reg.intercepts_[0]
+    encoder1 = (np.exp(encoder1) - np.exp(-encoder1)) / (np.exp(encoder1) + np.exp(-encoder1))
+
+    encoder2 = encoder1 * reg.coefs_[1] + reg.intercepts_[1]
+    encoder2 = (np.exp(encoder2) - np.exp(-encoder2)) / (np.exp(encoder2) + np.exp(-encoder2))
+
+    latent = encoder2 * reg.coefs_[2] + reg.intercepts_[2]
+    latent = (np.exp(latent) - np.exp(-latent)) / (np.exp(latent) + np.exp(-latent))
+
+    return np.asarray(latent)
+
+
+def plot_2d(
+        X: pd.DataFrame,
+        y: pd.Series,
+        save_dir: Path,
+) -> None:
+    """
+    Transform data in 2D and plot them.
+
+    Data are transformed in 2D via autoencoding.
+    The autoencoder is defined as a MLPRegressor with dimensions:
+        - n_input = 21
+        - encoding_layers = (10, 5)
+        - plot_dim = 2
+        - decoding_layers (10, 5)
+        - n_input = 21
+    To plot data, only encoding part is needed.
+
+    :param X: explanatory data as df
+    :param y: classes
+    :param save_dir: directory where to save results
+    """
+    logger.info("Plot data in 2D")
+
+    title = "2D encoded data"
+
+    # creating train test split
+    # y_train not used
+    X_train, X_test, _, y_test = train_test_split(
+        X, train_size=0.7, shuffle=True,
+    )
+
+    logger.info("Define autoencoding model.")
+    reg = MLPRegressor(
+        hidden_layer_sizes=(10, 5, 2, 5, 10),
+        activation='tanh',
+        solver='adam',
+        learning_rate_init=0.0001,
+        max_iter=20)
+
+    logger.info("Fitting model")
+    reg.fit(X_train, X_train)  # for autoencoders, same matrix is train & test
+
+    # define encoded variables
+    test_latent: np.ndarray = encoder(X_test, reg)
+
+    plt.figure(figsize=(10, 10))
+    for y_class in y_test.unique:
+        plt.scatter(
+            test_latent[np.argmax(y_test, axis=1) == y_class, 0],
+            test_latent[np.argmax(y_test, axis=1) == y_class, 1],
+            label=f'Class {y_class}')
+    plt.title(title)
+    plt.legend(fontsize=15)
+    plt.axis('equal')
+
+    # save image
+    image_path = save_dir / f"{title}.png"
+    plt.savefig(image_path)
+    plt.close()
+
+    # end
+    logger.info(f"Shapley summary info saved in {save_dir}")
+
+
+
